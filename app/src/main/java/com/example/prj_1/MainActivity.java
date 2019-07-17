fc928825ba0f69eefc638ec985305be33dd2a764
Andrej Timoshenko <32636772+Deveator@users.noreply.github.com>
2019-07-17 08:55:35 +0300

    Add ROI, change color via Canny filter

--- app/src/main/java/com/example/prj_1/MainActivity.java
+++ app/src/main/java/com/example/prj_1/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.prj_1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 import com.example.prj_1.CuView.DrawRect;
@@ -23,23 +27,39 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 import static com.example.prj_1.CuView.DrawRect.*;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
+import static org.opencv.imgproc.Imgproc.THRESH_TOZERO;
 
-public class MainActivity extends AppCompatActivity implements OnTouchListener {
+public class MainActivity extends AppCompatActivity {
 
     ImageView iV, iV2, iV3, iV4;
-    Button bttn, bttn2, bttn3;
+    public TextView tView;
+    Button bttn, bttn2, bttn3, bttn4, bttn5;
     public static String path;
-    Mat oImage, oImage2;
+    Mat oImage, oImage2, oImage3;
     static int mark = 0;
-    OneBall drawRect;
+    DrawRect drawRect;
     public static int x1, y1;
-    public static int widthDisp, heightDisp;
+    public static int widthDisp, heightDisp, screenWidth;
+    Bitmap bitmap;
+    private int thresholdValue = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,13 +67,16 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener {
         setContentView(R.layout.activity_main);
 
         iV = findViewById(R.id.imageV);
-        iV2 = findViewById(R.id.imageV2);
-        iV3 = findViewById(R.id.imageV3);
-        iV4 = findViewById(R.id.imageV4);
+    //    iV2 = findViewById(R.id.imageV2);
+   //     iV3 = findViewById(R.id.imageV3);
+    //    iV4 = findViewById(R.id.imageV4);
         bttn = findViewById(R.id.button1);
         bttn2 = findViewById(R.id.button2);
         bttn3 = findViewById(R.id.button3);
-          drawRect = findViewById(R.id.imageV5);
+        bttn4 = findViewById(R.id.button4);
+        bttn5 = findViewById(R.id.button5);
+        drawRect = findViewById(R.id.imageV5);
+        tView = findViewById(R.id.tW);
 
         System.loadLibrary("opencv_java3");
 
@@ -86,39 +109,30 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener {
             Uri imageUri = data.getData();
 
             path = getPath(imageUri);
+            System.out.println(00 - 00 - 00);
+            System.out.println(path);
+            // if()
 
-            oImage = OriginalImage.GetOriginalImage(path, 2, 2);
-            //  oImage2 = OriginalImage.GetOriginalImage(path, 4, 4);
+            //  oImage = OriginalImage.GetOriginalImage(path, 1, 1);
+            oImage = OriginalImage.GetOriginalImage2(path);
 
-            switch (mark) {
+                    //  Mat img = new Mat();
+                    /*
+                    Mat kern = new Mat(3, 3, CvType.CV_8S);
+                    int row = 0, col = 0;
+                    kern.put(row, col, 0, -1, 0, -1, 5, -1, 0, -1, 0);// '0, -1, 0, -1, 5, -1, 0, -1, 0' is dataSet to get 2D image
+                    //   Imgproc.cvtColor(mImg, mImg, Imgproc.COLOR_BGR2GRAY);
+                    Imgproc.filter2D(oImage3, oImage3, oImage3.depth(), kern);
 
-                case 0:
+                  ///  Imgproc.filter2D(oImage3, img, oImage3.depth(), kern);
+*/
+                    Imgproc.cvtColor(oImage, oImage, Imgproc.COLOR_BGR2GRAY);
+                    // Imgproc.Canny(oImage3, oImage3, 50, 100 * 2);
 
-                    // try to
-                    int thickness = -1;
-                    int lineType = 8;
-                    int shift = 0;
-                    Point center = new Point(400 / 2, 400 / 2);
-                    Imgproc.circle(oImage, center, 400 / 32, new Scalar(0, 0, 255), thickness, lineType, shift);
-
+                    /// Imgproc.cvtColor(oImage3, oImage3, Imgproc.COLOR_BGR2HSV);
                     displayImage(oImage);
-                    break;
-                case 1:
-                    Mat sMat = oImage.submat(200, 300, 0, 100);
-                    Imgproc.blur(sMat, sMat, new Size(3, 3));
-                    //     displayImage(setBlack(GrayImage.GetGrayImage(sMat)));
-                    break;
-                case 2:
-                    Mat sMat2 = oImage.submat(200, 300, 101, 290);
-                    Imgproc.blur(sMat2, sMat2, new Size(3, 3));
-                    //   displayImage(setBlack2(GrayImage.GetGrayImage(sMat2)));
-                    break;
-                case 3:
-                    Mat sMat3 = oImage.submat(100, 300, 291, 389);
-                    //   displayImage(FromWhiteToBlackBackgrnd.getFromWhiteToBlackBackgrnd(sMat3));
-                    break;
-            }
-            mark++;
+                    System.out.println(oImage.rows() + " rows");
+                    System.out.println(oImage.cols() + " clmns");
         }
     }
 
@@ -144,17 +158,21 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener {
 
     private void displayImage(Mat mat) {
 
-        Bitmap bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.RGB_565);
+        bitmap = Bitmap.createBitmap(mat.cols(), mat.rows(), Bitmap.Config.RGB_565);
 
         Utils.matToBitmap(mat, bitmap);
 
-        if (mark == 0) {
+      ///  if (mark == 0) {
             iV.setImageBitmap(bitmap);
             matImg1 = mat;
 
             System.out.println(matImg1.cols() + " cols");
             System.out.println(matImg1.rows() + " rows");
-        }
+            System.out.println(iV.getWidth() + " width");
+            System.out.println(iV.getHeight() + " height");
+            screenWidth = iV.getWidth();
+
+   //     }
 
         if (mark == 1) {
             iV2.setImageBitmap(bitmap);
@@ -169,7 +187,7 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener {
             matImg4 = mat;
         }
     }
-
+/*
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
@@ -192,10 +210,81 @@ public class MainActivity extends AppCompatActivity implements OnTouchListener {
         return true;
 
     }
+    */
+
 
     public void actionAny(View v) {
         DrawRect.getCoord(1);
     }
 
+    public void save(View view) {
+        // START creating directory for images
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/App_images");
+        if (!myDir.exists()) {
+            myDir.mkdir();
+        }
+        // STOP creating directory for images
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-" + n + ".jpg";
+        File file = new File(myDir, fname);
+        if (file.exists()) file.delete();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
+    public void transform(View view) {
+        // get coordinate s for subMat
+        DrawRect.getCoord(1);
+
+        // block to get coord os ROI
+        ArrayList<Integer> full_x_ROIcoord = new ArrayList<>();
+        ArrayList<Integer> full_y_ROIcoord = new ArrayList<>();
+
+        if (xRed < 0 || xRed > 750 || yRed < 0 || yRed > 1000 ||
+                xOrg < 0 || xOrg > 750 || yOrg < 0 || yOrg > 1000 ||
+                xYell < 0 || xYell > 750 || yYell < 0 || yYell > 1000 ||
+                xGreen < 0 || xGreen > 750 || yGreen < 0 || yGreen > 1000) {
+            return;
+        } else {
+            // clear situation with x<= xYell
+            for (int x = xRed; x < xYell; x++) {
+                for (int y = yRed; y < yYell; y++) {
+                    full_x_ROIcoord.add(x);
+                    full_y_ROIcoord.add(y);
+                }
+            }
+        }
+        // end block
+
+        // block get contour via CannyFilter
+        Mat sMat = oImage.submat(yRed, yGreen, xRed, xOrg);
+        Imgproc.Canny(sMat, sMat, 25, 100 * 2);
+        ArrayList<Double> subMatValue = new ArrayList<>();
+
+        for (int x = 0; x < sMat.cols(); x++) {
+            for (int y = 0; y < sMat.rows(); y++) {
+                double[] ft2 = sMat.get(y, x);
+                subMatValue.add(ft2[0]);
+            }
+        }
+        int count = 0;
+        for (int x = xRed; x < xYell; x++) {
+            for (int y = yRed; y < yYell; y++) {
+                oImage.put(y, x, subMatValue.get(count));
+                count++;
+            }
+        }
+        // end block
+
+        displayImage(oImage);
+    }
 }
